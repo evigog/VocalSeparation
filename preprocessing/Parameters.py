9522453e411048e02d812f1a6bc9e5927fa54458
Daniel del Castillo <ddcastilloiglesias@gmail.com>
2018-05-15 18:54:09 +0200

    Updated Parameters class

--- preprocessing/Parameters.py
+++ preprocessing/Parameters.py
@@ -2,4 +2,18 @@
 
 class Preprocessing:
     MAX_TIME = 4
-    NUM_COEF = 13
+    NCEPS = 13
+    NMELS = 40
+    PREEMPCOEFF = 0.97
+    FS = 16000
+    WINDOW = 'hann'
+    WINLEN = 512
+    WINSHIFT = WINLEN//2
+    NFFT = 512
+    DETREND = False
+    ONESIDED = True
+    BOUNDARY = None
+    PADDED = False
+
+class Postprocessing:
+    DENOISE_FACTOR = 5  # the larger it is, the smoother curve will be
